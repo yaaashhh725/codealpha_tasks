e882f02fdc250b0590edfe5e4e0ced83536ceda3
Yash Shinkar <shinkaryash025@gmail.com>
2025-02-14 15:56:52 +0530

    Commited task 1

---
+++ HangmanGame/main.py
@@ -0,0 +1,45 @@
+# Design a text-based Hangman game. The program
+# selects a random word, and the player guesses one
+# letter at a time to uncover the word. You can set a
+# limit on the number of incorrect guesses allowed.
+import random
+import nltk
+from nltk.corpus import words
+
+def choose_word():
+    nltk.download('words')
+    word_list = words.words()
+    return random.choice(word_list)
+
+def display_word(word, guessed_letters):
+    return " ".join(letter if letter in guessed_letters else "_" for letter in word)
+
+def hangman():
+    word = choose_word()
+    guessed_letters = set()
+    attempts = 6
+
+    print("Welcome to Hangman!")
+    
+    while attempts > 0:
+        print("\nWord:", display_word(word, guessed_letters))
+        print("Attempts left:", attempts)
+        guess = input("Guess a letter: ").lower()
+
+        if guess in guessed_letters:
+            print("You already guessed that letter.")
+        elif guess in word:
+            guessed_letters.add(guess)
+            print("Good job!")
+        else:
+            attempts -= 1
+            print("Incorrect guess.")
+
+        if all(letter in guessed_letters for letter in word):
+            print("\nCongratulations! You guessed the word:", word)
+            return
+    
+    print("\nGame over. The word was:", word)
+
+if __name__ == "__main__":
+    hangman()
